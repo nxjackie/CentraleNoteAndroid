ee0a8d22610e3f9e27e60046bbc247c69975502b
= <ama@via.ecp.fr>
2013-03-21 10:30:29 +0100

    Create intent between MainActivity and LoginActivity.

--- src/daron/centralenoteandroid/MainActivity.java
+++ src/daron/centralenoteandroid/MainActivity.java
@@ -1,7 +1,8 @@
 package daron.centralenoteandroid;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,8 +10,8 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+		Intent i = new Intent(this, LoginActivity.class);
+		startActivity(i); 
 	}
 
 	@Override
