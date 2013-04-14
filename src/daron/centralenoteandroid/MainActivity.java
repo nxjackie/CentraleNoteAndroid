ec277d807f23637dc8dcb922851f3523e07c6884
Nicolas LAURENT <nico.laurent@neuf.fr>
2013-04-15 01:12:34 +0200

    AddPerson is now ready to execute PHP code

--- src/daron/centralenoteandroid/MainActivity.java
+++ src/daron/centralenoteandroid/MainActivity.java
@@ -1,9 +1,7 @@
 package daron.centralenoteandroid;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -11,28 +9,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		SharedPreferences prefs = getPreferences(MODE_PRIVATE); 
-		String restoredText = prefs.getString("centraleNoteCircles", null);
-		
-		String circles;
-		
-		
-		if (restoredText == null) 
-		{
-			circles = "";
-		}
-		else
-		{
-			circles = restoredText;
-		}
-		
-		/*
-		To store in centraleNoteCircles
-		
-		SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
- 		editor.putString("centraleNoteCircles", "mon texte");
-		 */
 		
 		
 		
