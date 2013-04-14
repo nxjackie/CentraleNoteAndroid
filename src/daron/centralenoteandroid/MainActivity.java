f32557801f4329d4249e8931aa2ea4c6c342750c
Nicolas LAURENT <nico.laurent@neuf.fr>
2013-04-14 17:17:58 +0200

    Code for sharedPreferences in main
    Code to store and restore the value of centraleNoteCircles is comented
    in MainActivity.java

--- src/daron/centralenoteandroid/MainActivity.java
+++ src/daron/centralenoteandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package daron.centralenoteandroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,9 +12,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		SharedPreferences settings = getSharedPreferences("CentraleNote", 0);
+		SharedPreferences prefs = getPreferences(MODE_PRIVATE); 
+		String restoredText = prefs.getString("centraleNoteCircles", null);
 		
-		String cercles = settings.getString("Cercles", null);
+		String circles;
+		
+		
+		if (restoredText == null) 
+		{
+			circles = "";
+		}
+		else
+		{
+			circles = restoredText;
+		}
+		
+		/*
+		To store in centraleNoteCircles
+		
+		SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+ 		editor.putString("centraleNoteCircles", "mon texte");
+		 */
 		
 		
 		
